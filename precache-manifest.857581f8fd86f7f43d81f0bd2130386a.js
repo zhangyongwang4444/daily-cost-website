c5327dccacc288f11f0d8a92d70f4e668019fbf8
zhangyongwang <20225614@qq.com>
2020-03-12 22:24:25 +0800

    deploy

---
+++ precache-manifest.857581f8fd86f7f43d81f0bd2130386a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50dcaf0cb788247ae512",
+    "url": "/daily-cost-website/css/app.9964bdb0.css"
+  },
+  {
+    "revision": "9bf753bd0a8c4db2b58efa24972712a4",
+    "url": "/daily-cost-website/index.html"
+  },
+  {
+    "revision": "50dcaf0cb788247ae512",
+    "url": "/daily-cost-website/js/app.d3763928.js"
+  },
+  {
+    "revision": "43b0e175d158ec888ed2",
+    "url": "/daily-cost-website/js/chunk-vendors.2ba44f86.js"
+  },
+  {
+    "revision": "742a08c91078ab15e2015917c2fdd17f",
+    "url": "/daily-cost-website/manifest.json"
+  },
+  {
+    "revision": "ac12c3ebb1f58ad01ca63546fa425784",
+    "url": "/daily-cost-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/daily-cost-website/robots.txt"
+  }
+]);
